49e27fe7050d3fa342f8aa07d26b0e697b5d0a4b
Milind Mishra <milind.mishra4@gmail.com>
2024-07-20 11:38:09 +0530

    Initial Commit

---
+++ screenshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  screenshot
+//
+//  Created by Milind Mishra on 20/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ screenshot/screenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenshotApp.swift
+//  screenshot
+//
+//  Created by Milind Mishra on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct screenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
